38af944ba00b2ca34fca567ae9d98e82bde60eed
darryl <darrylmangibin@gmail.com>
2024-01-14 18:21:34 +0800

    Layout and meta title

---
+++ app/restaurant/[slug]/layout.tsx
@@ -0,0 +1,17 @@
+import React, { FC, PropsWithChildren } from 'react';
+import Header from './components/Header';
+
+export interface RestaurantLayoutProps extends PropsWithChildren {}
+
+const RestaurantLayout: FC<RestaurantLayoutProps> = ({ children }) => {
+	return (
+		<main>
+			<Header />
+			<div className='flex m-auto w-2/3 justify-between items-start 0 -mt-11'>
+				{children}
+			</div>
+		</main>
+	);
+};
+
+export default RestaurantLayout;
