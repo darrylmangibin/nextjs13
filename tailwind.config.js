720691af5e05d2fa9e2216039e2725274f4f56aa
darryl <darrylmangibin@gmail.com>
2024-01-14 15:36:41 +0800

    Add static pages

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./app/**/*.{tsx, js, jsx. ts}'],
+	theme: {
+		extend: {},
+		fontSize: {
+			'2xsm': '10px',
+			xsm: '12px',
+			sm: '13px',
+			lg: '18px',
+			'2xl': '22px',
+			'3xl': '25px',
+			'4xl': '32px',
+			'5xl': '40px',
+			'6xl': '22px',
+			'7xl': '70px',
+		},
+	},
+	plugins: [],
+};
